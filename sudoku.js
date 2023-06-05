f487f0e75cea57563d96f26f22087e81fada87a7
Sam Radford <smr1292@yahoo.com>
2023-06-05 15:16:20 -0400

    Added a second grid for solving while being able to reference first grid for starting state. Added input for setting starting state, which will also set solve grid to that for solving.  Display both given state and solve state for easy use in solving script repos

--- sudoku.js
+++ sudoku.js
@@ -1,30 +1,78 @@
-let state = [];
+// @ts-nocheck
+const stateDisplay = document.getElementById("stateDisplay");
+const solveStateDisplay = document.getElementById("solveStateDisplay");
+const startingInputs = [...document.querySelectorAll(".starting-cell")];
+const solveInputs = [...document.querySelectorAll(".solve-cell")];
+const initialStateInput = document.getElementById("startingStateInput");
+const initialStateInputButton = document.getElementById("startingStateInputButton");
+
+let initialState = [];
+let solveState = [];
+
+
 for(let idx = 0; idx < 81; idx++) {
-    state[idx] = 0;
+    initialState[idx] = 0;
+    solveState[idx] = 0;
 }
 
-const stateDisplay = document.getElementById("stateDisplay");
-
-const inputs = [...document.querySelectorAll("input")];
-inputs.forEach((input,index) => {
+startingInputs.forEach((input,index) => {
     input.addEventListener("input", (event) => {
-        inputChanged(event, index);
+        startInputChanged(event.target.value, index);
     });
 });
 
 
-function inputChanged(event, index) {
-    let value = event.target.value;
+function startInputChanged(value, index) {
     if(value >= 1 && value <= 9) {
-        state[index] = parseInt(value);
-        printState();
+        initialState[index] = parseInt(value);
+        printInitialState();
     } else if(value === "") {
-        state[index] = 0;
-        printState();
+        initialState[index] = 0;
+        printInitialState();
     }
 }
 
-
-function printState() {
-    stateDisplay.innerHTML = "State: " + JSON.stringify(state);
+function printInitialState() {
+    stateDisplay.innerHTML = "State: " + JSON.stringify(initialState);
 }
+
+solveInputs.forEach((input,index) => {
+    input.addEventListener("input", (event) => {
+        solveInputChanged(event.target.value, index);
+    });
+});
+
+function solveInputChanged(value, index) {
+    if(value >= 1 && value <= 9) {
+        solveState[index] = parseInt(value);
+        printSolveState();
+    } else if(value === "") {
+        solveState[index] = 0;
+        printSolveState();
+    }
+}
+
+function printSolveState() {
+    solveStateDisplay.innerHTML = "Solve State: " + JSON.stringify(solveState);
+}
+
+initialStateInputButton.addEventListener("click", (event) => {
+    let givenState = initialStateInput.value;
+    console.log("Got given state: " + givenState);
+    if(!givenState || givenState === "") { return; }
+    try{
+        let asJson = JSON.parse(givenState);
+        asJson.forEach((cellValue, index) => {
+            if(parseInt(cellValue) !== 0) {
+                startingInputs[index].value = cellValue;
+                startInputChanged(cellValue, index);
+
+                solveInputs[index].value = cellValue;
+                solveInputChanged(cellValue, index);
+            }
+        });
+    } catch(e) {
+        console.log(e);
+        console.log("^^^ Unable to convert input into grid");
+    }
+});
